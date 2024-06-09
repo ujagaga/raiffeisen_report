1703e007980e435a9c92a8509b05f56baa93615d
OOM PLS <ordinacija@drslavica.com>
2024-06-09 14:47:47 +0200

    Fixed credentials

--- raiffeisen_report.py
+++ raiffeisen_report.py
@@ -95,23 +95,26 @@ def list_mail(page_token=None):
     next_page_token = None
     msg_id_list = None
 
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+    try:
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+    except Exception as e:
+        print("ERROR authorizing:", e)
+        print("You probably need to download credentials from https://console.cloud.google.com/apis/credentials?project=mailreader-425811")
+        exit()
 
     try:
         service = build("gmail", "v1", credentials=creds)
